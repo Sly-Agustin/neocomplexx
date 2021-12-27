e5e3bc18a471a860794e320490c5702189a6eebb
Agustin Sly <agustin_sly@hotmail.com>
2021-12-27 14:44:56 -0300

    Manejo de errores
    - Actualizadas las validaciones
    - Ahora se detecta si algún atributo no es del tipo que se requiere
    - Agregado try-catch a la hora de insertar en la base de datos,  se capturan errores no contemplados y se devuelve un status 400.

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -15,14 +15,35 @@ async function newEmployee(req, res) {
 		});
 		return;
 	}
-    console.log(typeof(req.body.name));
-	let aux = await db.empleados.create({
-		nombre: req.body.name,
-		apellidoo: req.body.lastName,
-		cargo: req.body.position,
-		createdAt: new Date(),
-		updatedAt: new Date(),
-	});
+
+	if ((typeof(req.body.name) !== 'string') || (typeof(req.body.lastName) !== 'string') || (typeof(req.body.position) !== 'string')){
+		res.status(400).send({
+			message: 'Field content cant be a number, they must be characters',
+		});
+		return;
+	}
+
+	try {
+		let aux = await db.empleados.create({
+			nombre: req.body.name,
+			apellido: req.body.lastName,
+			cargo: req.body.position,
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		});
+	}
+	catch(err) {
+		if (err.parent.code == "ER_NO_DEFAULT_FOR_FIELD"){
+			res.status(400).send({
+				message: 'Not all fields have been filled.',
+				errorMessage: err.parent.sqlMessage
+			});
+		}
+		res.status(400).send({
+			message: 'An error has ocurred',
+			messageError: err.parent.sqlMessage,
+		});
+	}
 
 	return res.status(200).send({
 		message: 'Employee created successfully',
--- migrations/20211227170343-notNullLastname.js
+++ migrations/20211227170343-notNullLastname.js
@@ -3,16 +3,12 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn('empleados', 'nombre', {
+      type: Sequelize.STRING,
       allowNull: false
-    })
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    
   }
 };
